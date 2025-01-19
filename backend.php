848f9d784765b5d202deece249af658c86e95c8c
Shikhar0misra <143964077+Shikhar0misra@users.noreply.github.com>
2025-01-19 20:40:59 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $standCount = isset($_POST['standCount']) ? (float) $_POST['standCount'] : 0;
+    $rowLength = isset($_POST['rowLength']) ? (float) $_POST['rowLength'] : 0;
+    $rowSpacing = isset($_POST['rowSpacing']) ? (float) $_POST['rowSpacing'] : 0;
+
+    if ($standCount > 0 && $rowLength > 0 && $rowSpacing > 0) {
+        $rowSpacingFeet = $rowSpacing / 12; // Convert inches to feet
+        $plantsPerAcre = ($standCount / $rowLength) * (43560 / $rowSpacingFeet);
+        echo json_encode(["population" => round($plantsPerAcre)]);
+    } else {
+        echo json_encode(["error" => "Invalid input values"]);
+    }
+} else {
+    echo json_encode(["error" => "Invalid request method"]);
+}
+?>
